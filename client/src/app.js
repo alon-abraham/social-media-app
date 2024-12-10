93274523dff2e3468a10640e17202679928bb993
alon-abraham <alonabraham7@gmail.com>
2024-12-10 21:23:21 +0530

    added app.js

---
+++ client/src/app.js
@@ -0,0 +1,17 @@
+// /client/src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
